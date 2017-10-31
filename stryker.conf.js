9c509510ad0b82b3e506cf391d9bd9b2b6c3a5ba
Christoffer Kjeldgaard Petersen <christoffer@sappy.dk>
2017-10-31 15:08:53 +0100

    Updated dependencies

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,13 +1,12 @@
 module.exports = function(config){
-  config.set({
-    files: [
-        // Add your files here, this is just an example:
-        { pattern: 'src/**/*.js', mutated: true, included: true},
-        'tests/**/*.js',
-    ],
-    testRunner: 'jest',
-    testFramework: 'jasmine',
-    coverageAnalysis: 'perTest',
-    reporter: ['clear-text', 'progress'],
-  });
+    config.set({
+        files: [
+            // Add your files here, this is just an example:
+            { pattern: 'src/**/*.js', mutated: true, included: true},
+            'tests/**/*.js',
+        ],
+        testRunner: 'jest',
+        coverageAnalysis: 'all',
+        reporter: ['clear-text', 'progress'],
+    });
 }
